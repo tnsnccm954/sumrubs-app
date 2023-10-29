cf5a862f93a2e016dfd9e306cb3da988266be708
Tanasit Nimpradit <tns.nccm@gmail.com>
2023-10-29 15:57:46 +0700

    first commit

---
+++ resources/js/Components/utilities/SrNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <button
+        @click="
+            () => {
+                state.count++;
+            }
+        "
+    >
+        {{ state.count }}
+    </button>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+    setup() {
+        const state = reactive({
+            count: 0,
+        });
+
+        return {
+            ...toRefs(state),
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
