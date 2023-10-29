c283a61097d1ef64d007bd2e5997093e9e9ba912
Tanasit Nimpradit <tns.nccm@gmail.com>
2023-10-29 17:19:00 +0700

    make seeder command

---
+++ app/Console/Commands/MakeSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeSeeder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:sr-seeder {filename}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $filename = $this->argument('filename');
+        $prefixFn = "tns" . date('Y_m_d_His_');
+        $this->call('make:seed', ['name' => $prefixFn . $filename]);
+        return Command::SUCCESS;
+    }
+}
